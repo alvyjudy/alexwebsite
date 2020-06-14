8d1a657cfe1d65c0c8236ae9cdbd87d12a5ad20e
alvyjudy <alvyjudy@gmail.com>
2020-06-14 15:36:42 -0400

    isolate App component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
-function App () {
-  return (
-    <div>Hello wolrd</div>
-  );
-}
+import App from "./App";
 
 ReactDom.render(<App />, document.getElementById('root'))
