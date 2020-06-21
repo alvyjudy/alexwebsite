f69fd8ad1a3bb05d4d3db76aa6e1b01b35605bfc
赵书轩 <zhaoshuxuan@zhaoshuxuans-MacBook-Air.local>
2020-06-21 14:57:48 -0400

    small change to cart js file

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDom from'react-dom';
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 
+
 class Cart extends React.Component {
   render(){
-    return <div>Carttttttt</div>;
+    return(
+      <div>
+      <h1>cart</h1>
+      </div>
+    )
+
   }
 }
 
