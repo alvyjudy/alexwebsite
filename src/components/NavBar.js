93bb9e90f314ce352a4de2dc416df6f900e80500
alvyjudy <alvyjudy@gmail.com>
2020-06-22 17:32:12 -0400

    centralize routes into navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,31 +2,60 @@ import React from 'react';
 import ReactDom from'react-dom';
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import ItemContainer from './ItemContainer';
+import Item from './Item.js';
 import Cart from './Cart';
 
 import styles from '../stylesheets/NavBar.module.css';
+
+
+let itemId = 10000;
 
 class NavBar extends React.Component {
   insertRoutes() {
     return (
       <React.Fragment>
-        <Switch>
-          <Route exact path="/">
-            <ItemContainer content='top'/>
-          </Route>
+        <Route
+          exact path="/"
+          component={() => <ItemContainer content='top'/>}
+        />
 
-          <Route exact path="/aboutus">
-            <ItemContainer content='aboutus' />
-          </Route>
 
-          <Route exact path='/contact'>
-            <ItemContainer content='contact' />
-          </Route>
+        <Route
+          exact path="/aboutus"
+          component={() => <ItemContainer content='aboutus'/>}
+       />
 
-          <Route exact path="/cart">
-            <Cart />
-          </Route>
-        </Switch>
+
+        <Route
+          exact path='/contact'
+          component={() => <ItemContainer content='contact'/>}
+        />
+
+        <Route
+          exact path="/cart"
+          component={() => <Cart />}
+        />
+
+        <Route
+          exact path='/keychains'
+          component={() => <ItemContainer content='items' category='keychain' />}
+        />
+
+        <Route
+          exact path='/postcard'
+          component={() => <ItemContainer content='items' category='postcard' />}
+        />
+
+        <Route
+          exact path='/maskingtape'
+          component={() => <ItemContainer content='items' category='maskingtape' />}
+        />
+
+        <Route
+          exact path="/:itemId"
+          component={() => <Item content={itemId} />}
+        />
+
       </React.Fragment>
     )
   }
