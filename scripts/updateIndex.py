919047b6bdcdf305c98aa159f443373ea07d2dc2
alvyjudy <alvyjudy@gmail.com>
2020-06-30 18:18:07 -0400

    fixed dictionary reference bug

--- scripts/updateIndex.py
+++ scripts/updateIndex.py
@@ -6,7 +6,7 @@ from os.path import abspath, dirname, join
 
 HOST = "https://storage.googleapis.com/steeplehill"
 
-#contents = urllib.request.urlopen("https://storage.googleapis.com/steeplehill").read()
+
 tree = ET.parse('xmlfile.xml')
 root = tree.getroot()
 
@@ -22,9 +22,11 @@ for content in contents:
 
 index = {}
 index['categories'] = []
+
 for category in categories:
-    img_obj = {"name": None, 'url': None}
-    category_name = category.split("/")[1].split(".")[0] #keychain
+
+    category_name = category.split("/")[1].split(".")[0] #keychainr
+    #print(category_name)
     index[category_name] = []
 
     index['categories'].append(
@@ -33,11 +35,18 @@ for category in categories:
          }
     )
 
+
+
     for content in contents:
         if content.startswith(category_name):
+            img_obj = {}
             img_obj["name"] = content.split("/")[1].split(".")[0]
             img_obj["url"] = "/".join([HOST, content])
             index[category_name].append(img_obj)
+
+
+
+
 
 index_path = join(dirname(dirname(abspath(__file__))), 'src/mediaIndex.json')
 
