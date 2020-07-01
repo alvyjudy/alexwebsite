fdcbbeac1d3c0cda4546db12a3c5b9c084b861f4
unknown <ethan.liong@hotmail.com>
2020-07-01 13:06:02 -0400

    DB stuff

--- serve.js
+++ serve.js
@@ -1,12 +1,42 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require("mongoose");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("dist"));
 
 app.get("/", (req, res) => {
   res.sendFile("index.html");
 });
 
-app.listen(3001);
+app.post("/login", (req, res, next) => {
+  db.collection("people").findOne({ username: req.body[0] }, function (err, result) {
+    if (err) {
+      res.status(500).send("Error reading database.");
+      return;
+    }
+    if(!result){
+      res.send("no username");
+      return;
+    }
+    console.log(result)
+    if (result.pass == req.body[1]) {
+      res.send("Logged in successfully")
+      return;
+    }else{
+      res.send("Failed to login");
+      return;
+    }
+  });
+});
+
+mongoose.connect('mongodb://localhost/shop', { useNewUrlParser: true });
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  app.listen(3001);
+  console.log("Server listening on port 3001");
+});
--- src/components/ItemContainer.js
+++ src/components/ItemContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDom from'react-dom';
+import ReactDom from 'react-dom';
 import { BrowserRouter, Switch, Route, Link, useParams, Redirect } from 'react-router-dom';
 import Contact from './Contact.js';
 import styles from '../stylesheets/ItemContainer.module.css';
@@ -23,6 +23,8 @@ class ItemContainer extends React.Component {
         return this.renderContact();
       case "cart":
         return this.renderCart();
+      case "login":
+        return this.login();
       default:
         return this.renderItems();
     }
@@ -34,7 +36,7 @@ class ItemContainer extends React.Component {
       (imgObj, i) => {
         return (
           <Item
-            imgfile = {imgObj.url}
+            imgfile={imgObj.url}
             key={i}
             theme='category'
             category={imgObj.name}
@@ -57,7 +59,7 @@ class ItemContainer extends React.Component {
 
   renderContact() {
     return <div>
-    <Contact />
+      <Contact />
     </div>
   }
 
@@ -72,24 +74,28 @@ class ItemContainer extends React.Component {
             title={imgObj.name}
             key={i}
             theme='items'
-        />)
+          />)
       }
     )
     return (
-      <div>
+      <div className={styles.ItemContainer}>
         {allItems}
       </div>
     );
   }
-  
-  renderCart(){
+
+  renderCart() {
     let imgList = savedObj;
     console.log(savedObj)
     let allItems = imgList.map(
       (imgObj, i) => {
         return (
-          <div>
-            <img onClick={() => savedObj = savedObj.filter(rm => imgObj != rm, this.forceUpdate())} className={styles.Image} src={imgObj} />
+          <div className={styles.ItemCard}>
+            <img className={styles.ItemImage} src={imgObj} />
+            <button className={styles.Button} type='button' onClick={() => savedObj = savedObj.filter(rm => imgObj != rm, this.forceUpdate())}>
+              Remove from Cart
+              </button>
+
           </div>
         )
       }
@@ -100,11 +106,44 @@ class ItemContainer extends React.Component {
       </div>
     );
   }
+
+  login() {
+    return (
+      <div>
+        <label>
+          Name:
+          <br></br>
+          <input type="text" id="username" />
+        </label>
+        <br></br>
+        <label>
+          Password:
+          <br></br>
+          <input type="password" id="password" />
+        </label>
+        <br></br>
+        <button onClick={() => {
+          let req = new XMLHttpRequest();
+          req.onreadystatechange = function () {
+            if (this.readyState === 4) {
+              //redirect
+              console.log(this.response);
+            }
+          };
+          req.open("POST", "/login");
+          req.setRequestHeader("Content-Type", "application/json");
+          req.send(JSON.stringify([document.getElementById('username').value, document.getElementById('password').value]));
+        }}>
+          Login
+        </button>
+      </div>
+    )
+  }
 }
 
 
 
-class Item extends React.Component{
+class Item extends React.Component {
   constructor(props) {
     super(props);
   }
@@ -120,12 +159,12 @@ class Item extends React.Component{
 
   generateCategory() {
     return (
-        <Link to={`/items/${this.props.category}`} className={styles.Category}>
-          <p className={styles.TextInPic}>
-            {this.props.category}
-          </p>
-          <img src={this.props.imgfile} className={styles.Image} />
-        </Link>
+      <Link to={`/items/${this.props.category}`} className={styles.Category}>
+        <p className={styles.TextInPic}>
+          {this.props.category}
+        </p>
+        <img src={this.props.imgfile} className={styles.Image} />
+      </Link>
     )
   }
 
@@ -141,7 +180,7 @@ class Item extends React.Component{
         <button className={styles.Button} >Another button</button>
         <button className={styles.Button} type='button' onClick={
           () => savedObj.push(this.props.imgfile)
-          }>Add to cart</button>
+        }>Add to cart</button>
       </div>
     )
   }
