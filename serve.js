253b1e7055fae09979455cd6c6b0eb46a1ac054b
alvyjudy <alvyjudy@gmail.com>
2020-07-05 23:48:30 -0400

    quick hack for refresh to work

--- serve.js
+++ serve.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) => {
   res.sendFile("index.html");
 });
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/dist/index.html"));
+})
+
 /* to start the server
 $ export devMode='local' backEndIP=''
 $ node serve.js
