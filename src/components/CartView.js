95378bfad1061ec672fd7cfd4ac80e3191b342cb
alvy <alvyjudy@gmail.com>
2020-09-23 18:19:10 -0400

    shopping cart outlook refreshed

--- src/components/CartView.js
+++ src/components/CartView.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import itemsCatalog from "../itemsInfo.json";
 import styles from "../stylesheets/Cart.module.css";
 import {useDispatch, useSelector} from "react-redux";
@@ -12,39 +12,63 @@ const arrayToObj = (array, key) => {
     }, {})
 }
 
+const itemCatalogObj = arrayToObj(itemsCatalog, 'itemID');
+
 export const CartView = () => {
   const items = useSelector(state => state.cart);
   const itemsJSX = Object.values(items).map(
     (item) => {
-      const itemMeta = arrayToObj(itemsCatalog, 'itemID')[item.itemID];
       return <Item 
         key={item.itemID}
         itemID={item.itemID}
-        title={itemMeta.title}
-        url={itemMeta.url}
-        count={item.itemCount}
         />;
       }
   );
-  return <div className={styles.Container}>{itemsJSX}</div>
+
+  return <div className={styles.cart}>
+      <p className={styles.cartTitle}>Shopping Cart</p>
+      {itemsJSX.length === 0 ? <p className={styles.cartTitle}>Your Shopping Cart is empty</p> : undefined}
+
+      <div className={styles.Container}>{itemsJSX}</div>
+    </div>
+  
 }
 
 
-export const Item = ({itemID, url, count}) => {
+export const Item = ({itemID}) => {
   const dispatch = useDispatch();
+  const state = useSelector(state=>state.cart[itemID])
+  const {title, url, price} = itemCatalogObj[itemID];
+  const count = state.itemCount;
+
   return (
     <div className={styles.cartItem}>
+      <input type="checkbox" className={styles.itemCheckbox} />
       <img className={styles.cartItemImage} src={url} />
-      <p>{count}</p>
-      <button onClick={()=>{
-        dispatch(adjustItemCount(itemID, 1))
-      }}>+</button>
-      <button onClick={()=>{
-        dispatch(adjustItemCount(itemID, -1))
-      }}>-</button>
-      <button onClick={()=>{
-        dispatch(rmItemFromCart(itemID))
-      }}>Remove</button>
+      <p className={styles.itemTitle}>{title}</p>
+      <p className={styles.itemPrice}>{price} $CAD</p>
+
+      <div className={styles.itemQuantityBox}>
+        <p className={styles.itemCount}>Qty: {count}</p>
+
+        <button className={styles.itemCountButAdd}
+        onClick={()=>{
+          dispatch(adjustItemCount(itemID, 1))
+        }}>+</button>
+
+        <button className={styles.itemCountButMinus}
+        onClick={()=>{
+          dispatch(adjustItemCount(itemID, -1))
+        }}>-</button>
+
+        <button className={styles.removeBut}
+        onClick={()=>{
+          dispatch(rmItemFromCart(itemID))
+        }}>Remove</button>
+      
+      </div>
+
+      
     </div>
   )
 }
