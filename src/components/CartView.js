c0b9720dfc8137708945f8fe8e53a03dd3ece4c4
alvy <alvyjudy@gmail.com>
2020-09-24 11:22:16 -0400

    add itemcheckbox and subtotal calculation

--- src/components/CartView.js
+++ src/components/CartView.js
@@ -2,7 +2,12 @@ import React, {useState} from 'react';
 import itemsCatalog from "../itemsInfo.json";
 import styles from "../stylesheets/Cart.module.css";
 import {useDispatch, useSelector} from "react-redux";
-import {rmItemFromCart, adjustItemCount} from "../states/cart.actions";
+import {
+  rmItemFromCart, 
+  adjustItemCount,
+  uncheckItem,
+  checkItem,
+} from "../states/cart.actions";
 
 const arrayToObj = (array, key) => {
   return array.reduce(
@@ -16,6 +21,14 @@ const itemCatalogObj = arrayToObj(itemsCatalog, 'itemID');
 
 export const CartView = () => {
   const items = useSelector(state => state.cart);
+
+  const subtotal = Object.values(items).reduce(
+    (accu, item) => {
+      const itemPrice = itemCatalogObj[item.itemID].price * item.itemCount
+      return accu + (item.checked ? itemPrice : 0)
+    }, 0
+  )
+
   const itemsJSX = Object.values(items).map(
     (item) => {
       return <Item 
@@ -27,7 +40,13 @@ export const CartView = () => {
 
   return <div className={styles.cart}>
       <p className={styles.cartTitle}>Shopping Cart</p>
-      {itemsJSX.length === 0 ? <p className={styles.cartTitle}>Your Shopping Cart is empty</p> : undefined}
+
+      {itemsJSX.length === 0 ? 
+
+        <p className={styles.cartTitle}>Your Shopping Cart is empty</p> : 
+        <p className={styles.cartTitle}>Subtotal: {subtotal} CAD$</p>
+
+      }
 
       <div className={styles.Container}>{itemsJSX}</div>
     </div>
@@ -37,13 +56,25 @@ export const CartView = () => {
 
 export const Item = ({itemID}) => {
   const dispatch = useDispatch();
-  const state = useSelector(state=>state.cart[itemID])
+  const itemState = useSelector(state=>state.cart[itemID])
+
   const {title, url, price} = itemCatalogObj[itemID];
-  const count = state.itemCount;
+  const count = itemState.itemCount;
+  const checked = itemState.checked;
 
   return (
     <div className={styles.cartItem}>
-      <input type="checkbox" className={styles.itemCheckbox} />
+
+      <input 
+        type="checkbox" 
+        checked={checked}
+        className={styles.itemCheckbox} 
+        onChange={(e)=>{dispatch(
+          e.target.checked !== false ? 
+          checkItem(itemID) : 
+          uncheckItem(itemID)
+        )}}/>
+
       <img className={styles.cartItemImage} src={url} />
       <p className={styles.itemTitle}>{title}</p>
       <p className={styles.itemPrice}>{price} $CAD</p>
